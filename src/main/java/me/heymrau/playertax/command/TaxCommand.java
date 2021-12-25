4e6a3590a9e2c44d051b820070132337ec26645d
Osman Furkan <osmanfurkan115@gmail.com>
2021-12-25 22:46:46 +0300

    Fixed a bug

--- src/main/java/me/heymrau/playertax/command/TaxCommand.java
+++ src/main/java/me/heymrau/playertax/command/TaxCommand.java
@@ -5,7 +5,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class TaxCommand implements CommandExecutor {
@@ -17,18 +16,16 @@ public class TaxCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if(sender instanceof Player) {
-            if(!sender.hasPermission("tax.admin")) {
-                sender.sendMessage(ChatColor.RED + "Insufficient permission");
-                return true;
-            }
+        if (!sender.hasPermission("tax.admin")) {
+            sender.sendMessage(ChatColor.RED + "Insufficient permission");
+            return true;
+        }
 
-            if(args.length == 1 && args[0].equalsIgnoreCase("taxall")) {
-                taxManager.taxPlayers();
-                sender.sendMessage(ChatColor.GREEN + "You successfully taxed all players");
-            } else {
-                sender.sendMessage(ChatColor.GREEN + "Usage: /playertax taxall");
-            }
+        if (args.length == 1 && args[0].equalsIgnoreCase("taxall")) {
+            taxManager.taxPlayers();
+            sender.sendMessage(ChatColor.GREEN + "You successfully taxed all players");
+        } else {
+            sender.sendMessage(ChatColor.GREEN + "Usage: /playertax taxall");
         }
         return true;
     }
